dce5b94abbb4091298f03284e290c19a49347249
michal <bihone@wp.pl>
2017-04-07 10:41:40 +0200

    Update future readers controller tests (including ajax). Update controller and refactor code

--- test/controllers/future_readers_controller_test.rb
+++ test/controllers/future_readers_controller_test.rb
@@ -10,8 +10,21 @@ class FutureReadersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create future reader" do
     assert_difference("FutureReader.count", 1) do
-      post book_future_readers_url(@book)
+      assert_difference("Reader.count", -1) do
+        post book_future_readers_url(@book)
+      end
     end
+  end
+
+  test "should create future reader using ajax" do
+    assert_difference("FutureReader.count", 1) do
+      assert_difference("Reader.count", -1) do
+        post book_future_readers_url(@book), xhr: true
+      end
+    end
+    assert_match "Remove from wishlist", @response.body
+    assert_match "Already read it", @response.body
+    assert_equal "text/javascript", @response.content_type
   end
 
   test "should not create future reader if not logged in" do
@@ -32,6 +45,14 @@ class FutureReadersControllerTest < ActionDispatch::IntegrationTest
     assert_difference("FutureReader.count", -1) do
       delete book_future_reader_url(books(:two), @user)
     end
+  end
+
+  test "should delete future reader with ajax" do
+    assert_difference("FutureReader.count", -1) do
+      delete book_future_reader_url(books(:two), @user), xhr: true
+    end
+    assert_match "Add to wishlist", @response.body
+    assert_equal "text/javascript", @response.content_type
   end
 
   test "should not delete if not logged in" do
