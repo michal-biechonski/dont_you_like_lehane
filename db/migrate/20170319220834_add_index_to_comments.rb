1ee3336738ec248373aa9efe11ea7708c8cd47e1
michal <bihone@wp.pl>
2017-04-07 21:28:03 +0200

    Fix add index to comments, readers and future readers migrations

--- db/migrate/20170319220834_add_index_to_comments.rb
+++ db/migrate/20170319220834_add_index_to_comments.rb
@@ -1,6 +1,6 @@
 class AddIndexToComments < ActiveRecord::Migration[5.0]
   def change
-    add_index :comments, :book_id
-    add_foreign_key :comments, :book_id
+    add_index :comments, :book
+    add_foreign_key :comments, :book
   end
 end
--- db/migrate/20170323184906_add_indexes_to_readers_and_future_readers.rb
+++ db/migrate/20170323184906_add_indexes_to_readers_and_future_readers.rb
@@ -1,6 +1,6 @@
 class AddIndexesToReadersAndFutureReaders < ActiveRecord::Migration[5.0]
   def change
-    add_index :readers, [:user_id, :book_id], unique: true
-    add_index :future_readers, [:user_id, :book_id], unique: true
+    add_index :readers, [:user, :book], unique: true
+    add_index :future_readers, [:user, :book], unique: true
   end
 end
