255436ebbd17f6ae023303b79cff4f5edffc7ee3
michal <bihone@wp.pl>
2017-04-17 17:06:21 +0200

    Add avatars to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170326093455) do
+ActiveRecord::Schema.define(version: 20170417142812) do
 
   create_table "books", force: :cascade do |t|
     t.string   "author",               default: "Dennis Lehane"
@@ -83,6 +83,10 @@ ActiveRecord::Schema.define(version: 20170326093455) do
     t.datetime "locked_at"
     t.string   "name"
     t.boolean  "admin",                  default: false
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
