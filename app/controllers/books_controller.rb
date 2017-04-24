8ed795b789fbd8ff59ed54c35026871c6c228df0
michal <bihone@wp.pl>
2017-04-24 19:51:20 +0200

    Refactor books and comments controllers based on guidelines

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,10 @@
+# HANDLE ALL BOOKS WRITTEN BY LEHANE
 class BooksController < ApplicationController
-  before_action :set_user, only: [:new, :create, :edit, :update, :destroy]
-  before_action :require_admin, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_user, only: %i[new create edit update destroy]
+  before_action :require_admin, only: %i[new create edit update destroy]
   before_action :set_reader, only: [:show]
   before_action :set_future_reader, only: [:show]
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: %i[show edit update destroy]
 
   # GET /books
   # GET /books.json
@@ -23,17 +24,17 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /books
   # POST /books.json
   def create
     @book = Book.new(book_params)
-
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.html do
+          redirect_to @book, notice: 'Book was successfully created.'
+        end
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
@@ -47,7 +48,9 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.html do
+          redirect_to @book, notice: 'Book was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
@@ -61,21 +64,21 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully deleted.' }
+      format.html do
+        redirect_to books_url, notice: 'Book was successfully deleted.'
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def book_params
-      params.require(:book).permit(:title, :description, :picture, :published_at)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
 
-
-
+  # Never trust parameters from the internet, only allow the white list through.
+  def book_params
+    params.require(:book).permit(:title, :description, :picture, :published_at)
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
+# HANDLE ALL COMMENTS FROM USERS
 class CommentsController < ApplicationController
-  
   before_action :set_user
-  before_action :set_book, only: [:new, :create, :destroy]
+  before_action :set_book, only: %i[new create destroy]
   before_action :set_comment, only: [:destroy]
   before_action only: [:destroy] do
     same_user(@comment)
@@ -15,31 +15,30 @@ class CommentsController < ApplicationController
     @comment = @book.comments.build(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      flash[:notice] = "Comment was successfully added."
+      flash[:notice] = 'Comment was successfully added.'
       redirect_to @book
     else
-      render "new"
+      render 'new'
     end
   end
 
   def destroy
     @comment.destroy
-    flash[:notice] = "Comment was successfully deleted."
+    flash[:notice] = 'Comment was successfully deleted.'
     redirect_to @book
   end
 
   private
 
-    def set_book
-      @book = Book.find(params[:book_id])
-    end
+  def set_book
+    @book = Book.find(params[:book_id])
+  end
 
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
-    def comment_params
-      params.require(:comment).permit(:content)
-    end
-
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
