587089bcef70a74965e7e0eb4b73f9c4740d5bc4
michal <bihone@wp.pl>
2017-04-24 19:31:55 +0200

    Refactor code in application controller based on guidelines

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,44 +1,44 @@
+# APPLICATION CONTROLLER
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def set_reader
-    if user_signed_in?
-      set_book
-      @exist_reader = Reader.where("user_id = ? AND book_id = ?", current_user.id, @book.id).take
-    end
+    return unless user_signed_in?
+    set_book
+    @exist_reader = Reader.where('user_id = ? AND book_id = ?',
+                                 current_user.id, @book.id).take
   end
 
   def set_future_reader
-    if user_signed_in?
-      set_book
-      @fut_reader = FutureReader.where("user_id = ? AND book_id = ?", current_user.id, @book.id).take
-    end
+    return unless user_signed_in?
+    set_book
+    @fut_reader = FutureReader.where('user_id = ? AND book_id = ?',
+                                     current_user.id, @book.id).take
   end
 
   def set_user
     if user_signed_in?
       @user = current_user
     else
-      flash[:alert] = "You must be logged in to do that."
+      flash[:alert] = 'You must be logged in to do that.'
       redirect_to new_user_session_path
     end
   end
 
   def require_admin
-    unless current_user.admin?
-      flash[:alert] = "Only administrators have access to do this task."
-      redirect_to users_path
-    end
+    return if current_user.admin?
+    flash[:alert] = 'Only administrators have access to do this task.'
+    redirect_to users_path
   end
 
   def same_user(resource)
-    unless current_user.admin? || (current_user.id == resource.user_id)
-      resource == @comment ? type = "comments" : type = "posts"
-      flash[:alert] = "You can only do this to your own #{type}!"
-      redirect_to users_path
-    end
+    return if current_user.admin? || (current_user.id == resource.user_id)
+    type = if resource == @comment
+             'comments'
+           else
+             'posts'
+           end
+    flash[:alert] = "You can only do this to your own #{type}!"
+    redirect_to users_path
   end
-
-
-
 end
