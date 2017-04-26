20d9dc4afe5eeb681e88a62bbce7d19573a633db
michal <bihone@wp.pl>
2017-04-26 21:14:45 +0200

    Refactor application, books and devise helper based on guidelines. Delete blank helper files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,27 +1,12 @@
+# UNIVERSAL HELPERS
 module ApplicationHelper
-
   def show_avatar(user, height = 100)
     gravatar(user, height)
   end
 
   def gravatar(user, size)
-    id = Digest::MD5::hexdigest(user.email.downcase)
+    id = Digest::MD5.hexdigest(user.email.downcase)
     path = "https://secure.gravatar.com/avatar/#{id}?s=#{size}&d=mm"
     image_tag(path, alt: user.name)
   end
-
-  def already_read_this?
-    @read = Reader.where("user_id = ? AND book_id = ?", current_user.id, @book.id)
-    unless @read.empty?
-      return true
-    end
-  end
-
-  def wants_to_read_this?
-    @future_read = FutureReader.where("user_id = ? AND book_id = ?", current_user.id, @book.id)
-    unless @future_read.empty?
-      return true
-    end
-  end
-
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,14 @@
+# HELPERS FOR BOOKS CONTROLLER
 module BooksHelper
+  def already_read_this?
+    @read = Reader.where('user_id = ? AND book_id = ?',
+                         current_user.id, @book.id)
+    return true unless @read.empty?
+  end
+
+  def wants_to_read_this?
+    @future_read = FutureReader.where('user_id = ? AND book_id = ?',
+                                      current_user.id, @book.id)
+    return true unless @future_read.empty?
+  end
 end
