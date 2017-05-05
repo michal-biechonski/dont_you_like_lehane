13b78ec3018bbd4eeda9ddd72d9a900201f5c13c
michal <bihone@wp.pl>
2017-05-05 18:45:58 +0200

    Update assets and devise initializers - ruby guidelines

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,9 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = "1.0"
 
-Rails.application.config.assets.precompile += %w(pages.css)
+Rails.application.config.assets.precompile += %w[pages.css]
 # Rails.application.config.assets.precompile += %w(users.css)
 # Rails.application.config.assets.precompile += %w(books.css)
 # Rails.application.config.assets.precompile += %w(posts.css)
