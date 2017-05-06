219c6afa7b35e182c3afc221023b0c9c2a62443f
michal <bihone@wp.pl>
2017-05-06 17:38:12 +0200

    Update config files - application, environment and spring.

--- config/application.rb
+++ config/application.rb
@@ -8,17 +8,15 @@ Bundler.require(*Rails.groups)
 
 module DontYouLikeLehane
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
+    # Settings in config/environments/* take precedence over
+    # those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  
-  # Match .scss, .sass or .less comments. Example // TODO make some notes
-  config.annotations.register_extensions("scss", "sass", "less", "haml") do |annotation|
-    /\/\/\s*(#{annotation}):?\s*(.*)$/
-  end
-
-  config.action_view.embed_authenticity_token_in_remote_forms = true
-
-
+    # Match .scss, .sass or .less comments. Example // TODO make some notes
+    config.annotations.register_extensions("scss", "sass", "less",
+                                           "haml") do |annotation|
+      %r{/\/\/\s*(#{annotation}):?\s*(.*)$/}
+    end
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
