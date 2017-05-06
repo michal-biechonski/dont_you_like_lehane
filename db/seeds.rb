6a3c1814e033b4d3b3ed428de44edbef0ab5e99f
michal <bihone@wp.pl>
2017-05-06 17:45:50 +0200

    Update config file - puma and db file - seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to
+# seed the database with its default values.
+# The data can then be loaded with the rails db:seed command
+# (or created alongside the database with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name: 'Star Wars' },
+# { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-lehaneBooks = [
+lehane_books = [
 
-{
-title: "A Drink Before the War",
-description: "Private Investigators Kenzie and Gennaro are tasked to retrieve missing documents by a trio of politicians. The trail leads them into the midst of a gang war and reveals an act of child abuse. Kenzie struggles with memories of his own past while Gennaro deals with her abusive marriage.",
-published_at: 23.years.ago
-},
-{
-title: "Darkness, Take My Hand",
-description: "When Detectives Patrick Kenzie and Angela Gennaro agree to protect the son of a prominent psychiatrist they soon find bodies are piling up around them. What's more, all the clues point to an unlikely suspect - a serial killer who has been in prison for twenty years, so he can't be killing again, can he?
+  {
+    title: "A Drink Before the War",
+    description: "Private Investigators Kenzie and Gennaro are tasked to|
+     retrieve missing documents by a trio of politicians. The trail leads|
+      them into the midst of a gang war and reveals an act of child abuse.|
+       Kenzie struggles with memories of his own past while Gennaro deals|
+        with her abusive marriage.",
+    published_at: 23.years.ago
+  },
+  {
+    title: "Darkness, Take My Hand",
+    description: "When Detectives Patrick Kenzie and Angela Gennaro agree|
+     to protect the son of a prominent psychiatrist they soon find bodies|
+      are piling up around them. What's more, all the clues point to an|
+       unlikely suspect - a serial killer who has been in prison for twenty|
+       years, so he can't be killing again, can he?
 
-As the duo try to find out what kind of human being could perform such horrifying acts of mutilation, torture and dismemberment, they discover that the killer's motive is disturbingly rooted in their own past. In a series of heart-stopping climaxes that grow ever more bloody, ever more terrifying, the two detectives work frantically to capture the killer before they become victims themselves.",
-published_at: 21.years.ago
-},
-{
-title: "Sacred",
-description: "Patrick Kenzie and Angela Gennaro are hired by a dying billionaire to find his daughter, Desiree, after the previous detective working the case, Jay Becker, disappeared.",
-published_at: 20.years.ago
-},
-{
-title: "Gone, Baby, Gone",
-description: "Boston-based lovers and private investigators Patrick Kenzie and Angie Gennaro are hired by a woman to look into the case of her niece, Amanda McCready, whose disappearance has become an important local news story. They take the case despite the seeming reluctance of the girl's uncle, Lionel. During the investigation they quickly come to the conclusion that Amanda's mother, Helene, who has been prominently featured in the news stories about the case, is a degenerate and neglectful parent. At the time of Amanda's disappearance, Helene had left her alone for several hours while she partied at a local dive bar. ",
-published_at: 19.years.ago
-}
+    As the duo try to find out what kind of human being could perform|
+    such horrifying acts of mutilation, torture and dismemberment,|
+    they discover that the killer's motive is disturbingly rooted in|
+    their own past. In a series of heart-stopping climaxes that grow|
+     ever more bloody, ever more terrifying, the two detectives work|
+      frantically to capture the killer before they become victims themselves.",
+    published_at: 21.years.ago
+  },
+  {
+    title: "Sacred",
+    description: "Patrick Kenzie and Angela Gennaro are hired by a dying|
+     billionaire to find his daughter, Desiree, after the previous|
+      detective working the case, Jay Becker, disappeared.",
+    published_at: 20.years.ago
+  },
+  {
+    title: "Gone, Baby, Gone",
+    description: "Boston-based lovers and private investigators Patrick |
+    Kenzie and Angie Gennaro are hired by a woman to look into the |
+    case of her niece, Amanda McCready, whose disappearance has become |
+    an important local news story. They take the case despite the |
+    seeming reluctance of the girl's uncle, Lionel. During the investigation |
+    they quickly come to the conclusion that Amanda's mother, Helene, who has |
+    been prominently featured in the news stories about the case, is a |
+    degenerate and neglectful parent. At the time of Amanda's |
+    disappearance, Helene had left her alone for several hours while she |
+    partied at a local dive bar. ",
+    published_at: 19.years.ago
+  }
 ]
 
-lehaneBooks.each do |book|
+lehane_books.each do |book|
   Book.create(book)
 end
 
@@ -41,7 +68,10 @@ end
   email = name + "@example.com"
   password = "password"
   password_confirmation = "password"
-  User.create(name: name, email: email, password: password, password_confirmation: password_confirmation)
+  User.create(name: name,
+              email: email,
+              password: password,
+              password_confirmation: password_confirmation)
 end
 
 Book.first(4).each do |book|
@@ -65,4 +95,3 @@ end
 # Comment.all.each do |comment|
 #   comment.destroy
 # end
-
