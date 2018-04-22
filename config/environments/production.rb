c1687d3aaaf021baff23499823f88d63147f9cbc
Michal <bihone88@gmail.com>
2018-04-22 22:31:16 +0000

    Update host in config environment production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "dontyoulikelehane-michall8888.herokuapp.com"
+  host = "dontyoulikelehane-michal8888.herokuapp.com"
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address:              "smtp.sendgrid.net",
