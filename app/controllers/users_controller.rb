89c597c555a3b0c9c4f3e7d138338feaf299c90f
Michał Biechoński <michal.biechonski@netguru.pl>
2018-07-29 15:48:09 +0200

    Use decent exposure in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,22 @@
 # USER ACTIONS NOT HANDLED BY DEVISE
 class UsersController < ApplicationController
-  before_action :set_user_params, only: %i[show destroy]
+  expose :users, -> { User.all }
+  expose :user
+
   before_action :require_logged_in, only: [:destroy]
   before_action :require_admin, only: [:destroy]
 
-  def index
-    @users = User.all
-  end
-
-  def show; end
-
   def destroy
-    @user.destroy
-    flash[:notice] = "User was successfully deleted."
+    user.destroy
+    flash[:notice] = 'User was successfully deleted.'
     redirect_to users_path
   end
 
   private
 
-  def set_user_params
-    @user = User.find(params[:id])
-  end
-
   def require_logged_in
     return if signed_in?
-    flash[:alert] = "You must be logged in to do that."
+    flash[:alert] = 'You must be logged in to do that.'
     redirect_to new_user_session_path
   end
 end
