f277376a2024e9216095ca36fea5f095e0dcaf49
Michał Biechoński <michal.biechonski@netguru.pl>
2018-07-29 21:51:42 +0200

    Extract method from view to model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ApplicationRecord
   belongs_to :book
   belongs_to :user
   validates :content, presence: true
+
+  def belongs_to_user?(user)
+    user_id == user&.id || user&.admin?
+  end
 end
